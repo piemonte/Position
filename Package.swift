760606c22e8dee7ede5943b4120950c6b6bd4d3f
patrick piemonte <patrick.piemonte@gmail.com>
2021-06-22 13:41:38 -0700

    update package manager config

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "Position",
     platforms: [
-      .iOS(.v12)
+      .iOS(.v13)
     ],
     products: [
       .library(name: "Position", targets: ["Position"])
